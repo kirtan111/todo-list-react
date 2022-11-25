82870050889ecac900bf80be507dec7079430800
kirtan111 <kirtanthummar7103@protonmail.com>
2022-11-25 13:41:59 +0530

    First Commit

---
+++ src/components/Form.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
+export const Form = (props) => {
+    const [name, setName] = useState("");
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.addTask("Done!");
+        setName("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                placeholder="Add Task"
+                value={name}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+        </form>
+    );
+};
